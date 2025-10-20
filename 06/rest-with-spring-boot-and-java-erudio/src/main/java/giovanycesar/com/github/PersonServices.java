3be104f2306d68e1ee87c79809192a93f8f1e644
Giovany Almeida <gvncesar@gmail.com>
2025-10-20 14:10:42 -0300

    @Service e @Autowired

---
+++ 06/rest-with-spring-boot-and-java-erudio/src/main/java/giovanycesar/com/github/PersonServices.java
@@ -0,0 +1,31 @@
+package giovanycesar.com.github;
+
+import giovanycesar.com.github.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one person");
+
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Giovany");
+        person.setLastName("Almeida");
+        person.setAddress("Fortaleza - CE");
+        person.setGender("Male");
+
+        return person;
+    }
+
+
+}
