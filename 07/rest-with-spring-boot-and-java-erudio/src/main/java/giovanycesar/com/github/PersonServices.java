8afe8895ed3f95e90794f3db1a952518e6f44f81
Giovany Almeida <gvncesar@gmail.com>
2025-10-22 18:32:56 -0300

    Definindo propriedades de conexão

---
+++ 07/rest-with-spring-boot-and-java-erudio/src/main/java/giovanycesar/com/github/PersonServices.java
@@ -0,0 +1,76 @@
+package giovanycesar.com.github;
+
+import giovanycesar.com.github.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding one people.");
+
+
+        List<Person> persons = new ArrayList<Person>();
+
+        for (int i = 0; i < 8; i++) {
+
+            Person person = mockPerson(i);
+
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some Address " + i);
+        person.setGender("Male");
+
+        return person;
+    }
+
+    public Person findById(String id) {
+        logger.info("Finding one person.");
+
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Giovany");
+        person.setLastName("Almeida");
+        person.setAddress("Fortaleza - CE");
+        person.setGender("Male");
+
+        return person;
+    }
+
+
+    public Person create(Person person) {
+        logger.info("Creating one person");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one person.");
+    }
+}
---
+++ 07/rest-with-spring-boot-and-java-erudio/src/main/java/giovanycesar/com/github/exception/ExceptionResponse.java
@@ -0,0 +1,6 @@
+package giovanycesar.com.github.exception;
+
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) {
+}
