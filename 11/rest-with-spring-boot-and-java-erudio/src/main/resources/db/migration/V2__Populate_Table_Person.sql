26ced7f3085acaac5185e3d2c649bf79513ed8f1
Giovany Almeida <gvncesar@gmail.com>
2025-10-31 17:47:14 -0300

    Suporte à Migrations com Flyway

---
+++ 11/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brazil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Fortaleza - CE', 'Giovany', 'Male', 'César de Almeida'),
+	(4, 'Fortaleza - CE', 'Giovanna', 'Female', 'Souza'),
+	(6, 'Fortaleza - CE', 'Giovanna', 'Female', 'Maria'),
+	(7, 'Fortaleza - CE', 'Jane', 'Female', 'de Souza');
