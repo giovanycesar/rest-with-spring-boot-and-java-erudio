34eb72c66fd39b806291d61f4fda59b3171147a2
Giovany Almeida <gvncesar@gmail.com>
2026-03-31 12:34:11 -0300

    Content Negotiation via Query Parameter e Header Parameter

---
+++ 13/rest-with-spring-boot-and-java-erudio/src/main/java/giovanycesar/com/github/config/WebConfig.java
@@ -0,0 +1,38 @@
+package giovanycesar.com.github.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // Via EXTENSION _.xml _.json Deprecated on Spring Boot 2.6
+
+        /*
+        // Via Query Param _?mediaType=xml
+
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+         */
+
+        // Via Query Param _?mediaType=xml
+
+        configurer.favorParameter(false)
+                  .ignoreAcceptHeader(false)
+                  .useRegisteredExtensionsOnly(false)
+                  .defaultContentType(MediaType.APPLICATION_JSON)
+                  .mediaType("json", MediaType.APPLICATION_JSON)
+                  .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
